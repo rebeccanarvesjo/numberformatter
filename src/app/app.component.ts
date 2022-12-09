997c8fa1c8c1b3fb04b01a3cd6f15576118e7549
rebeccanarvesjo <rebecca.n.dev@gmail.com>
2022-12-09 13:07:12 +0100

    Initial commit, with full working solution.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'number-formatter';
+  formattedNumber: any;
+  separator: any;
+  inputNumber: any;
+
+  constructor() {
+    this.separator = 2;
+  }
+
+  formatNumber() {
+    //console.log('change occurred!');
+    //console.log(form.value);
+
+    let displayArray: string[] = [];
+    let cleanedStr: string = '';
+    cleanedStr = this.inputNumber.replace(/[^a-zA-Z0-9 ]/g, '');
+    //console.log(cleanedStr);
+    let numArray = [...cleanedStr];
+
+    const stringLength = numArray.length;
+
+    let currElem = '';
+
+    for (let i: number = 0; i <= stringLength; i += 2) {
+      //console.log('i: ', i);
+      if (this.separator == 5) {
+        break;
+      }
+
+      if (numArray.length >= this.separator) {
+        currElem += numArray.splice(0, this.separator).join('');
+        displayArray.push(currElem);
+        currElem = '';
+      }
+    }
+    if (this.separator != 5) {
+      if (numArray.length > 0) {
+        let lastSplice = numArray.splice(0, numArray.length).join('');
+        displayArray.push(lastSplice);
+      }
+    }
+
+    if (this.separator == 5) {
+      for (let i = numArray.length; i > 0; i--) {
+        if (numArray.length < 3) {
+          let lastSlice = numArray.splice(0, numArray.length).join('');
+          displayArray.splice(0, 0, lastSlice);
+          break;
+        } else {
+          let slice1 = numArray.splice(-3, 3).join('');
+          displayArray.splice(0, 0, slice1);
+        }
+      }
+    }
+
+    this.formattedNumber = displayArray;
+  }
+  title = 'Number Formatter';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
